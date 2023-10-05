19847a7b0790159954de43689d0aa6bdc5ded671
unknown <daniel.hopp1@gmail.com>
2023-10-05 16:50:42 +0200

    Finishing LSTM modelling

--- datathon/modelling_helper.py
+++ datathon/modelling_helper.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
-def min_max_scale_df(df, reference_df):
+def min_max_scale_df(df, reference_df, unscale = False):
     """
     Min-max scale a dataframe
     parameters:
         :df: pd.DataFrame: the dataframe to min-max scale according to the reference dataframe. The first column will not be scaled (i.e., the date)
         :reference_df: pd.DataFrame: the reference dataframe to compute min and maxes (e.g., the training data)
+        :unscale: bool: False to min max scale, True to rescale to original units
     output:
         :pd.DataFrame: min-max scaled dataframe
     """    
     scaled_df = df.copy()
-    
-    for col in df.columns[1:]:
-        scaled_df[col] = (df[col] - reference_df[col].min()) / (reference_df[col].max() - reference_df[col].min())
+    if not(unscale):
+        for col in df.columns[1:]:
+            scaled_df[col] = (df[col] - reference_df[col].min()) / (reference_df[col].max() - reference_df[col].min())
+    else:
+        for col in df.columns[1:]:
+            scaled_df[col] = (df[col] * (reference_df[col].max() - reference_df[col].min())) + reference_df[col].min()
         
     return scaled_df
--- modelling.py
+++ modelling.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 from nowcast_lstm.LSTM import LSTM
 from nowcast_lstm.model_selection import variable_selection, hyperparameter_tuning, select_model
 
@@ -7,10 +9,9 @@ from nowcast_lstm.model_selection import variable_selection, hyperparameter_tuni
 from datathon.modelling_helper import min_max_scale_df
 
 data = pd.read_csv("data/data.csv", parse_dates = ["date"])
-
 target = "exports_usd"
+
 train_start = "2019-01-01"
-#validation_start = "2021-12-01"
 test_start = "2023-01-01"
 
 train = data.loc[lambda x: (x.date >= train_start) & (x.date < test_start), :].reset_index(drop = True)
@@ -19,30 +20,32 @@ train = min_max_scale_df(train, unscaled_train)
 test = data.loc[lambda x: (x.date >= train_start), :].reset_index(drop = True)
 test = min_max_scale_df(test, unscaled_train)
 
-chosen_model = select_model(
-    data = train,
-    target_variable = target,
-    n_models = 5,
-    n_timesteps_grid = [1],
-    train_episodes_grid = [50, 100],
-    batch_size_grid = [int(len(train)/2), int(len(train)/4)],
-    n_hidden_grid = [10, 20, 40],
-    n_layers_grid = [1, 2, 4],
-    n_folds = 3,
-    init_test_size = 0.5,
-    initial_ordering = "feature_contribution"
-)
+# set to true if performing model selection
+if False:
+    chosen_model = select_model(
+        data = train,
+        target_variable = target,
+        n_models = 5,
+        n_timesteps_grid = [1],
+        train_episodes_grid = [50, 100],
+        batch_size_grid = [int(len(train)/2), int(len(train)/4)],
+        n_hidden_grid = [10, 20, 40],
+        n_layers_grid = [1, 2, 4],
+        n_folds = 3,
+        init_test_size = 0.5,
+        initial_ordering = "feature_contribution"
+    )
+    
+    print(chosen_model.variables.values[0])
+    print(chosen_model.hyperparameters.values[0])
 
-print(chosen_model.variables.values[0])
-print(chosen_model.hyperparameters.values[0])
-
-# chosen hyperparams + variables
-# ['crude_brent', 'vostochny', 'primorsk']
-# {'n_models': 5, 'n_timesteps': 1, 'fill_na_func': <function nanmean at 0x00000208A9D86CA0>, 'fill_ragged_edges_func': <function nanmean at 0x00000208A9D86CA0>, 'train_episodes': 100, 'batch_size': 12, 'decay': 0.98, 'n_hidden': 40, 'n_layers': 2, 'dropout': 0, 'criterion': '', 'optimizer': '', 'optimizer_parameters': {'lr': 0.01}}
-
-variables = chosen_model.variables.values[0]
+# model inference
+variables = ["crude_brent", "vostochny", "primorsk"]
+train = train.loc[: , ["date", target] + variables]
+test = test.loc[: , ["date", target] + variables]
+unscaled_train = unscaled_train.loc[: , ["date", target] + variables]
 model = LSTM(
-    data = train.loc[:, ["date", target] + variables],
+    data = train,
     target_variable = target,
     n_models = 20,
     n_timesteps = 1,
@@ -53,4 +56,25 @@ model = LSTM(
 )
 model.train()
 
-model.predict(test.loc[:, ["date", target] + variables]).set_index("date").plot()
+preds = model.predict(test)
+
+# unscaling
+unscaled_actuals = test.copy()
+unscaled_actuals = min_max_scale_df(test, unscaled_train, unscale = True)
+unscaled_preds = test.copy()
+unscaled_preds = unscaled_preds.drop(target, axis = 1).merge(preds.loc[:, ["date", "predictions"]], on = "date", how = "left").rename(columns = {"predictions": target})
+unscaled_preds = min_max_scale_df(unscaled_preds, unscaled_train, unscale = True)
+
+# plotting
+final_df = pd.DataFrame({
+    "date": unscaled_actuals.date,
+    "actuals": unscaled_actuals[target],
+    "predictions": unscaled_preds[target]
+})
+
+final_df.to_csv("predictions.csv", index = False)
+
+final_df.set_index("date").plot()
+plt.axvline(pd.to_datetime(test_start))
+plt.savefig("plot.png")
+
